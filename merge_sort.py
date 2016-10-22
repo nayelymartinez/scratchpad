4a99131eac0be54bc4f0cb31e62e1e4031c8ab0e
Nayely <nayely.e.martinez@gmail.com>
2016-10-22 03:13:25 -0400

    Correct divide incorrect merge

--- merge_sort.py
+++ merge_sort.py
@@ -1,20 +1,50 @@
 import math
 
-def merge_sort(lst, p, q, r):
-	if len(lst) <= 2:
+def merge_sort(merged, lst, p, r):
+	if len(lst) <= 1:
+		print("Less or equal to 1", lst)
 		return lst
-	else:
-		#q = int(math.floor((p-r))/2)
+	else:	
+		q = len(lst)//2
 		l_lst = lst[p:q]
 		r_lst = lst[q:r]
 		print("L:",l_lst)
 		print("R:",r_lst)
-		return merge(merge_sort(l_lst, 0, int(math.floor(q-p)/2), q), merge_sort(r_lst, q, int(math.floor(r-q)/2), int(math.floor(len(lst)/2))))
+		print("p:", p)
+		print("q:", q)
+		print("r:", r, "\n")
+		left_to_sort = merge_sort(merged, l_lst, 0, len(l_lst))
+		right_to_sort = merge_sort(merged, r_lst, 0, len(r_lst))
+		print("\n ** Ready to merge **")
+		print("Left to merge:", left_to_sort)
+		print("Right to merge:", right_to_sort)
+		merged = merge(merged, left_to_sort, right_to_sort)
+		print("Merged so far:",merged, "\n")
+		return merged
 
-def merge(lst1, lst2):
+def merge(merged_lst, lst1, lst2):
 	t1 = 0
 	t2 = 0
-	merged_lst = []
+	print("Lst1 to merge:", lst1)
+	print("Lst2 to merge:", lst2)
+	while t1 <= len(lst1) and t2 <= len(lst2):
+		if lst1[t1] <= lst2[t2]:
+			merged_lst.append(lst1[t1])
+			t1 += 1
+			# Add remaining list2 items (if any) if reached end of list1
+			if t1 >= len(lst1):
+				merged_lst.extend(lst2[t2:])
+				return merged_lst
+		else:
+			merged_lst.append(lst2[t2])
+			t2 += 1
+			if t2 >= len(lst2):
+				merged_lst.extend(lst1[t1:])
+				return merged_lst
+
+def merge_it(merged_lst,lst1, lst2):
+	t1 = 0
+	t2 = 0
 	print("Lst1 to merge:", lst1)
 	print("Lst2 to merge:", lst2)
 	while (t1 < len(lst1)) and (t2 < len(lst2)):
@@ -38,12 +68,12 @@ def merge(lst1, lst2):
 
 def main():
 	int_lst = [
-		[8, 2, 11, 7, 1, 5, 6, 4],
-		[], 
-		[4],
-		[1, 5, 2]
+		#[8, 2, 11, 7, 1, 5, 6, 4],
+		#[], 
+		#[4],
+		[1, 5, 2, 4]
 	]
-	print(merge_sort(int_lst[0], 0, len(int_lst[0])//2, len(int_lst[0])))
+	print("Final merged list:",merge_sort([],int_lst[0], 0, len(int_lst[0])-1))
 
 if __name__ == '__main__':
 	main()
