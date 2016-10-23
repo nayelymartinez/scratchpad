9d572ac229d6cf9e776da6145212fc3552985d6d
Nayely <nayely.e.martinez@gmail.com>
2016-10-22 22:09:53 -0400

    Working mergesort

--- merge_sort.py
+++ merge_sort.py
@@ -1,79 +1,70 @@
-import math
-
-def merge_sort(merged, lst, p, r):
+def merge_sort(lst):
 	if len(lst) <= 1:
-		print("Less or equal to 1", lst)
+		#print("Equals 1:", lst)
 		return lst
-	else:	
-		q = len(lst)//2
-		l_lst = lst[p:q]
-		r_lst = lst[q:r]
-		print("L:",l_lst)
-		print("R:",r_lst)
-		print("p:", p)
-		print("q:", q)
-		print("r:", r, "\n")
-		left_to_sort = merge_sort(merged, l_lst, 0, len(l_lst))
-		right_to_sort = merge_sort(merged, r_lst, 0, len(r_lst))
-		print("\n ** Ready to merge **")
-		print("Left to merge:", left_to_sort)
-		print("Right to merge:", right_to_sort)
-		merged = merge(merged, left_to_sort, right_to_sort)
-		print("Merged so far:",merged, "\n")
+	else:
+		p = 0
+		r = len(lst)
+		q = (r-p)//2
+		left_lst = lst[p:q]
+		right_lst = lst[q:r]
+		#print("Left lst:", left_lst)
+		#print("Right lst:", right_lst, "\n")
+		left_sorted = merge_sort(left_lst)
+		right_sorted = merge_sort(right_lst)
+		#print("Left list to be merged:", left_sorted)
+		#print("Right list to be merged:", right_sorted)
+		merged = merge(left_sorted, right_sorted)
+		#print("Merged so far:",merged)
 		return merged
 
-def merge(merged_lst, lst1, lst2):
+def merge(lst1, lst2):
+	#print("\n ** Ready to merge **")
+	#print("Lst1 to be merged:", lst1)
+	#print("Lst2 to be merged:", lst2)
+	#print("\n")
 	t1 = 0
 	t2 = 0
-	print("Lst1 to merge:", lst1)
-	print("Lst2 to merge:", lst2)
-	while t1 <= len(lst1) and t2 <= len(lst2):
+	merged = []
+	while t1 < len(lst1) and t2 < len(lst2):
+		#print("T1:", t1, "and t2:", t2)
 		if lst1[t1] <= lst2[t2]:
-			merged_lst.append(lst1[t1])
+			merged.append(lst1[t1])
 			t1 += 1
-			# Add remaining list2 items (if any) if reached end of list1
-			if t1 >= len(lst1):
-				merged_lst.extend(lst2[t2:])
-				return merged_lst
+			if t1 == len(lst1) and t2 <= len(lst2):
+				merged.extend(lst2[t2:])
+				#print("So far:", t1, t2, merged)
+				break
 		else:
-			merged_lst.append(lst2[t2])
+			merged.append(lst2[t2])
 			t2 += 1
-			if t2 >= len(lst2):
-				merged_lst.extend(lst1[t1:])
-				return merged_lst
+			if t2 == len(lst2) and t1 <= len(lst1):
+				merged.extend(lst1[t1:])
+				#print("So far:", t1, t2, merged)
+				break
+		#print("So far:", t1, t2, merged)
+	return merged
 
-def merge_it(merged_lst,lst1, lst2):
-	t1 = 0
-	t2 = 0
-	print("Lst1 to merge:", lst1)
-	print("Lst2 to merge:", lst2)
-	while (t1 < len(lst1)) and (t2 < len(lst2)):
-		if lst1[t1] <= lst2[t2]:
-			merged_lst.append(lst1[t1])
-			print("Merged:",merged_lst)
-			t1 += 1
-			if t1 >= len(lst1):
-				merged_lst.append(lst2[t2])
-				print("Merged:",merged_lst)
-				return merged_lst
-		else:
-			merged_lst.append(lst2[t2])
-			print("Merged:",merged_lst)
-			t2 += 1
-			if t2 >= len(lst2):
-				merged_lst.append(lst1[t1])
-				print("Merged:",merged_lst)
-				return merged_lst
-		
+def test():
+	lsts = [
+		[],
+		[4],
+		[9,2],
+		[8, 2, 11, 7, 1, 5, 6, 4]
+	]
+	for l in lsts:
+		print("\nInput (unsorted):", l)
+		print("Output (sorted):",merge_sort(l))
+
+	assert merge_sort(lsts[0]) == [], "Did not sort correctly"
+	assert merge_sort(lsts[1]) == [4], "Did not sort correctly"
+	assert merge_sort(lsts[2]) == [2,9], "Did not sort correctly"
+	assert merge_sort(lsts[3]) == [1,2,4,5,6,7,8,11], "Did not sort correctly"
 
 def main():
-	int_lst = [
-		#[8, 2, 11, 7, 1, 5, 6, 4],
-		#[], 
-		#[4],
-		[1, 5, 2, 4]
-	]
-	print("Final merged list:",merge_sort([],int_lst[0], 0, len(int_lst[0])-1))
+	test()
+	return
+
 
 if __name__ == '__main__':
 	main()
