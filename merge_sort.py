2e498a3750537fb5e6e1e44efe3fa02568c71867
Nayely <nayely.e.martinez@gmail.com>
2016-10-22 22:12:01 -0400

    Added description + removed comments

--- merge_sort.py
+++ merge_sort.py
@@ -1,6 +1,10 @@
+# Merge Sort implementation
+# Author: Nayely Martinez
+# Date: 10/22/16 
+# Description: Recursive merge sort implementation in Python
+
 def merge_sort(lst):
 	if len(lst) <= 1:
-		#print("Equals 1:", lst)
 		return lst
 	else:
 		p = 0
@@ -8,41 +12,28 @@ def merge_sort(lst):
 		q = (r-p)//2
 		left_lst = lst[p:q]
 		right_lst = lst[q:r]
-		#print("Left lst:", left_lst)
-		#print("Right lst:", right_lst, "\n")
 		left_sorted = merge_sort(left_lst)
 		right_sorted = merge_sort(right_lst)
-		#print("Left list to be merged:", left_sorted)
-		#print("Right list to be merged:", right_sorted)
 		merged = merge(left_sorted, right_sorted)
-		#print("Merged so far:",merged)
 		return merged
 
 def merge(lst1, lst2):
-	#print("\n ** Ready to merge **")
-	#print("Lst1 to be merged:", lst1)
-	#print("Lst2 to be merged:", lst2)
-	#print("\n")
 	t1 = 0
 	t2 = 0
 	merged = []
 	while t1 < len(lst1) and t2 < len(lst2):
-		#print("T1:", t1, "and t2:", t2)
 		if lst1[t1] <= lst2[t2]:
 			merged.append(lst1[t1])
 			t1 += 1
 			if t1 == len(lst1) and t2 <= len(lst2):
 				merged.extend(lst2[t2:])
-				#print("So far:", t1, t2, merged)
 				break
 		else:
 			merged.append(lst2[t2])
 			t2 += 1
 			if t2 == len(lst2) and t1 <= len(lst1):
 				merged.extend(lst1[t1:])
-				#print("So far:", t1, t2, merged)
 				break
-		#print("So far:", t1, t2, merged)
 	return merged
 
 def test():
