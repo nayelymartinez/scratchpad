309a4024b0cec9b06c3c5cff2c569d4c89aba93b
Nayely <nayely.e.martinez@gmail.com>
2016-11-02 19:48:05 -0400

    Added description

--- house_robber.py
+++ house_robber.py
@@ -4,14 +4,15 @@
 # Description: Recursive solution to house robber problem. 
 # Given a list of values for a neighborhood of "houses", return the maximum value if no two adjacent houses (values) in the list can be robbed. 
 
-def robber(house_lst, opt1 = 0, opt2 = 0):
+def robber(house_lst, maximum, opt1 = 0, opt2 = 0):
 	if len(house_lst) == 1:
 		return house_lst[0]
 	else:
-		opt1 += robber(house_lst[1:])
+		opt1 += robber(house_lst[1:], maximum)
 		opt2 += house_lst[0]
 		#print(opt1, opt2)
-		return max(opt1, opt2)
+		maximum += max(opt1, opt2)
+
 
 def main():
 	house_lst = [4, 5, 6]
